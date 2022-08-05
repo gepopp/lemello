0bf6ba48148558b1a03afebcce69c704159d9143
gepopp <gerhard@poppgerhard.at>
2022-08-05 21:03:12 +0200

    wip

--- app/Http/Controllers/SubscriptionsController.php
+++ app/Http/Controllers/SubscriptionsController.php
@@ -2,44 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\Authenticate;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use App\Models\Account;
+use Laravel\Cashier\Cashier;
 use Stripe\StripeClient;
 use Illuminate\Http\Request;
 
 class SubscriptionsController extends Controller
 {
 
-    public function subscription(Account $account){
-        dd($account);
+    public function subscription(Account $account)
+    {
+
+        $subscription = $account->subscriptions()->where('name', 'default')->active()->first();
+        $stripe_subscription = Cashier::stripe()->subscriptions->retrieve($subscription->stripe_id);
+
+        $pay = 0;
+        foreach ($stripe_subscription->items->data as $item) {
+            $pay += $item->price->unit_amount;
+        }
+
+
+        return Inertia::render('Subscription', [
+            'account' => $account,
+            'subscription' => [
+                'next_bill' => (new Carbon($stripe_subscription->current_period_end))->format('d.m.y'),
+                'amount' => $pay,
+                'payment_method' => Cashier::stripe()->paymentMethods->retrieve($stripe_subscription->default_payment_method)
+            ]
+        ]);
+
     }
 
 
-     public function susbcribe(){
-
-         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-         $stripe = new StripeClient( env('STRIPE_SECRET'));
-
-         $customer = \Illuminate\Support\Facades\Auth::user()->account->createOrGetStripeCustomer();
-
-         $subscription = $stripe->subscriptions->create([
-             'customer' => \Illuminate\Support\Facades\Auth::user()->account->stripe_id,
-             'items' => [[
-                             'price' => "price_1LRkwyHs0uYxWjsAu9gXpQpf",
-                         ]],
-             'payment_behavior' => 'default_incomplete',
-             'payment_settings' => ['save_default_payment_method' => 'on_subscription'],
-             'expand' => ['latest_invoice.payment_intent'],
-         ]);
+    public function susbcribe()
+    {
 
 
-         return Inertia::render( 'Subscribe', [
-             'key' => env( 'STRIPE_KEY' ),
-             'customer' => $customer,
-             'subscriptionId' => $subscription->id,
-             'client_secret' => $subscription->latest_invoice->payment_intent->client_secret,
-             'redirect' => \route('dashboard')
-         ] );
+        if (Auth::user()->account->subscribed('default')) {
+            return redirect(route('subscription', ['account' => Auth::user()->account]));
+        }
 
-     }
+
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+        $stripe = new StripeClient(env('STRIPE_SECRET'));
+
+        $customer = \Illuminate\Support\Facades\Auth::user()->account->createOrGetStripeCustomer();
+
+        $subscription = $stripe->subscriptions->create([
+            'customer' => \Illuminate\Support\Facades\Auth::user()->account->stripe_id,
+            'items' => [[
+                'price' => "price_1LRkwyHs0uYxWjsAu9gXpQpf",
+            ]],
+            'payment_behavior' => 'default_incomplete',
+            'payment_settings' => ['save_default_payment_method' => 'on_subscription'],
+            'expand' => ['latest_invoice.payment_intent'],
+        ]);
+
+
+        return Inertia::render('Subscribe', [
+            'key' => env('STRIPE_KEY'),
+            'customer' => $customer,
+            'subscriptionId' => $subscription->id,
+            'client_secret' => $subscription->latest_invoice->payment_intent->client_secret,
+            'redirect' => \route('dashboard')
+        ]);
+
+    }
 }
--- resources/js/Pages/Subscription.vue
+++ resources/js/Pages/Subscription.vue
@@ -3,20 +3,25 @@ import AppLayout from '@/Layouts/AppLayout.vue';
 import {onMounted, ref} from "vue";
 import {usePage} from "@inertiajs/inertia-vue3";
 
-const subscriptions = ref();
+const account = usePage().props.value.account;
+const subscriptions = usePage().props.value.subscription;
+
 </script>
 
 <template>
     <AppLayout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dein Abo
+                Dein Abo test
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
+                   <pre>
+                       <code v-text="subscriptions"></code>
+                   </pre>
                 </div>
             </div>
         </div>
